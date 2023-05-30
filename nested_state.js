23ef4be42a7a1a01b81b96eed0c9e2edec011853
Jmytwenty8 <jmy2898@gmail.com>
2023-05-30 13:59:49 +0530

    Nested State Barebones

---
+++ nested_state.js
@@ -0,0 +1,42 @@
+const { createStore } = require("redux");
+const nested_state = {
+  name: "Rick and Morty",
+  address: {
+    street: "lubalubalubdub",
+    city: "Council of Rick",
+    state: "Georgia",
+  },
+};
+
+const STREET_UPDATED = "STREET_UPDATED";
+
+const updateStreet = (payload) => {
+  return {
+    type: STREET_UPDATED,
+    payload: payload,
+  };
+};
+
+const reducer = (state = nested_state, action) => {
+  switch (action.type) {
+    case STREET_UPDATED:
+      return {
+        ...state,
+        address: {
+          ...state.address,
+          street: action.payload,
+        },
+      };
+    default: {
+      return state;
+    }
+  }
+};
+
+const store = createStore(reducer);
+
+store.subscribe(() => {
+  console.log("Updated State ", store.getState());
+});
+
+store.dispatch(updateStreet("North Carolina"));
