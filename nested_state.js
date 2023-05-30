ca8aa93f089992ae2e6dd19123845f877e976e42
Jmytwenty8 <jmy2898@gmail.com>
2023-05-30 14:04:52 +0530

    Immer Produce

--- nested_state.js
+++ nested_state.js
@@ -1,4 +1,5 @@
 const { createStore } = require("redux");
+const { produce } = require("immer");
 const nested_state = {
   name: "Rick and Morty",
   address: {
@@ -20,13 +21,17 @@ const updateStreet = (payload) => {
 const reducer = (state = nested_state, action) => {
   switch (action.type) {
     case STREET_UPDATED:
-      return {
-        ...state,
-        address: {
-          ...state.address,
-          street: action.payload,
-        },
-      };
+      //   return {
+      //     ...state,
+      //     address: {
+      //       ...state.address,
+      //       street: action.payload,
+      //     },
+      //   };
+
+      return produce(state, (draft) => {
+        draft.address.street = action.payload;
+      });
     default: {
       return state;
     }
